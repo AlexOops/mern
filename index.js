a243c30df986f8709e343b2374d46d1675020e25
Alex <8768905213@mail.ru>
2023-03-22 12:56:19 +0300

    upload and refactoring

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 
-import {registerValidation, loginValidation, postCreateValidation} from "./validations.js"
+import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
 
-import checkAuth from "./utils/checkAuth.js";
+import {checkAuth, handleValidationErrors} from "./utils/index.js";
 
-import * as UserController from "./controllers/UserController.js"
-import * as PostController from "./controllers/PostController.js";
+import {UserController, PostController} from "./controllers/index.js";
 
 mongoose
     // .connect('mongodb+srv://admin:12345@testdb.i3vxtxl.mongodb.net/?retryWrites=true&w=majority')
     .connect('mongodb://localhost:27017/blog')
     .then(() => {
-        console.log('Db has been connected!')
+        console.log('Db has been connected!');
     })
     .catch((err) => {
-        console.log("The problem with connecting...", err)
+        console.log("The problem with connecting...", err);
     })
 
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+app.use(multer({storage: storage}).single("image"));
+
+app.use('/uploads', express.static('uploads'));
 
 app.use(express.json());
 
@@ -26,21 +39,32 @@ app.get("/", (req, res) => {
     res.send("Hello world");
 })
 
-app.post("/auth/register", registerValidation, UserController.register)
-app.post("/auth/login", loginValidation, UserController.login)
-app.get("/auth/me", checkAuth, UserController.getMe)
+app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.get("/posts", PostController.getAll)
-app.get("/posts/:id", PostController.getOne)
-app.post("/posts", checkAuth, postCreateValidation, PostController.create)
-app.delete("/posts/:id", checkAuth, PostController.remove)
-app.patch("/posts/:id", checkAuth, PostController.update)
+app.post("/upload", checkAuth, (req, res) => {
+    let data = req.file;
+    if (!data)
+        res.send("failed to upload file");
+    else
+        res.json({
+            message: "file uploaded",
+            url: `/uploads/${req.file.originalname}`
+        })
+});
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.patch("/posts/:id", checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+app.delete("/posts/:id", checkAuth, PostController.remove);
 
 app.listen(4444, (err) => {
     if (err) {
-        return console.log(err)
+        return console.log(err);
     } else {
-        console.log("Server has been started!")
+        console.log("Server has been started!");
     }
 })
 
