a6e7c72b02765e14c00111344a150e7ada0caf60
Alex <8768905213@mail.ru>
2023-03-22 10:54:35 +0300

    CRUD block

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,7 +4,7 @@ export const create = async (req, res) => {
     try {
         const doc = new PostModel({
             title: req.body.title,
-            text: req.body.title,
+            text: req.body.text,
             tags: req.body.tags,
             imageUrl: req.body.imageUrl,
             user: req.userId // при создании id же будет в req
@@ -42,23 +42,59 @@ export const getOne = async (req, res) => {
             },
             {
                 returnDocument: 'after' // вернуть новый документ после обновления
-            },
-            (err, doc) => { // функция, которая будет выполняться
-                if (err) {
-                    console.log(err);
-                    return res.status(500).json({message: "failed to get post!"});
-                }
+            })
+            .then((doc) => { // функция, которая будет выполняться
                 if (!doc) {
-                    console.log(err);
                     return res.status(404).json({message: "post will not be found"});
                 }
-
                 res.json(doc);
-            }
-        );
+            });
 
     } catch (err) {
         console.log(err);
         res.status(500).json({message: "failed to get post!"});
+    }
+}
+
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndDelete(
+            {
+                _id: postId
+            })
+            .then((doc) => {
+                if (!doc) {
+                    return res.status(404).json({message: "post will not be removed"});
+                }
+                res.json({success: true});
+            });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "failed to get post!"});
+    }
+}
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.updateOne(
+            {
+                _id: postId
+            },
+            {
+                title: req.body.title,
+                text: req.body.text,
+                tags: req.body.tags,
+                imageUrl: req.body.imageUrl,
+                user: req.userId
+            })
+            .then(res.json({success: true}));
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "failed to update post!"});
     }
 }
